9df913569f607f304d96050d434e75e702d06e39
kingsley etoka <etokakingsley@gmail.com>
2023-08-08 07:47:51 +0100

    created strategy files

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './users.repository';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
   async create(createUserDto: CreateUserDto) {
     return this.userRepository.create(createUserDto);
+  }
+
+  async verifyUser(email: string, password: string) {
+    console.log(password);
+    const user = await this.userRepository.findOne({ email });
+    // const passwordIsValid = await bcrypt.compare(password, user.password);
+    // if (!passwordIsValid) {
+    //   throw new UnauthorizedException('Credentials are not valid.');
+    // }
+    return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.userRepository.findOne(getUserDto);
   }
 }
